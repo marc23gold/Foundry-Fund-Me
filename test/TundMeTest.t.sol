87f51f9f77c5939cc304774bdec0c752f1087079
Marcx <haitianhero6@gmail.com>
2023-07-17 02:53:30 -0400

    refactored to make contract testing and deployment more modular

--- test/TundMeTest.t.sol
+++ test/TundMeTest.t.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol"; 
 
-contract FundMeTest is Test, FundMe{
+contract FundMeTest is Test{
     FundMe fundMe;
     function setUp() external {
         // set up test environment
-        fundMe = new FundMe();
-    }
+       DeployFundMe deployFundMe = new DeployFundMe();
+       fundMe = deployFundMe.run();
+    }   
 
     function testMinimumDollarIsFive() public {
        assertEq(fundMe.MINIMUM_USD(), 5e18);
