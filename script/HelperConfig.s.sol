47c30091c3cd626f27ae1efce671bbe4519cf12c
Marcx <haitianhero6@gmail.com>
2023-07-28 16:23:03 -0400

    tests

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script{
  /*
@@ -19,21 +20,27 @@ contract HelperConfig is Script{
         if(block.chainid  == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory) { 
         NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
         return sepoliaConfig;
     }
-    function getAnvilEthConfig() public returns(NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+        if(activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
         //price feed address
         /*
         *Deploy mocks
         *Return the mock address 
         */
        vm.startBroadcast(); 
-       
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
        vm.stopBroadcast();
+
+       NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+       return anvilConfig;
     }
 }
--- test/TundMeTest.t.sol
+++ test/TundMeTest.t.sol
@@ -8,10 +8,16 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
     FundMe fundMe;
+    address USER = makeAddr("user");
+    uint constant ETH = 0.1 ether;
+    uint constant START = 10 ether;
+
+
     function setUp() external {
         // set up test environment
        DeployFundMe deployFundMe = new DeployFundMe();
        fundMe = deployFundMe.run();
+       vm.deal(USER, START);
     }   
 
     function testMinimumDollarIsFive() public {
@@ -27,6 +33,43 @@ contract FundMeTest is Test{
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public payable {
+        vm.expectRevert(); //next line should revert
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); //next tx will be sent by user
+        fundMe.fund{value:ETH}();
+        uint amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, ETH);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER); //next tx will be sent by user
+        fundMe.fund{value:ETH}();
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER); 
+    }
+
+    modifier funded() {
+        vm.prank(USER); //next tx will be sent by user
+        fundMe.fund{value:ETH}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.prank(USER); //next tx will be sent by user
+        vm.expectRevert(); //next line should revert
+        fundMe.withdraw();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        //Arrange
+        //Act
+        //Assert 
     }
 
 }
